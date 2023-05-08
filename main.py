7c22838874caa3c394fd90969001f74c12f92bd0
1antonioleite1 <antonioleitefilho02@gmail.com>
2023-05-08 10:56:29 -0300

    Exercício Concluido

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ num  =  int(input("Dígite um número inteiro:"))
 if num < 0 or num == 0:
     print("Número inválido")
 else:
-    primo = True
-    for i in range(2,num):
+    a = 0
+    for i in range(2,num  ):
         if num % i == 0:
-            print("Não primo")
-            primo = False
+            a = a + 1
+    if a > 0 or num == 1:
+        print("Não primo")
 
-    
+    else:
+        print("Primo")
             
 
